76d27c51e6672ebc102010b5fb61b862c6067d0d
Fareez Ahmed <fareez.ahmed@ge.com>
2016-06-20 20:28:57 -0400

    finished objects

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,18 @@
 exports = typeof window === 'undefined' ? global : window;
-
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
-
+  alterContext: function (fn, obj) {
+    return fn.call(obj);
   },
-
-  alterObjects: function(constructor, greeting) {
-
+  alterObjects: function (constructor, greeting) {
+    constructor.prototype.greeting = greeting;
   },
-
-  iterate: function(obj) {
-
+  iterate: function (obj) {
+    var returnedValuesAndKeys = [];
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        returnedValuesAndKeys.push(key + ': ' + obj[key]);
+      }
+    }
+    return returnedValuesAndKeys;
   }
 };
