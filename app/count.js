bf2b3666c9482c6ecf5e56350c15defad62d0119
Fareez Ahmed <fareez.ahmed@ge.com>
2016-06-21 10:05:06 -0400

    finish timer/count

--- app/count.js
+++ app/count.js
@@ -1,7 +1,19 @@
 exports = typeof window === 'undefined' ? global : window;
-
 exports.countAnswers = {
   count: function (start, end) {
+    var timer;
 
+    function doCount() {
+      console.log(start++);
+      if (start <= end) {
+        timer = setTimeout(doCount, 100);
+      }
+    }
+    doCount();
+    return {
+      cancel: function () {
+        timer && clearTimeout(timer);
+      }
+    };
   }
 };
