2030c35ef43e49300213a60b41a6c35efa339ca4
Fareez Ahmed <fareez.ahmed@ge.com>
2016-06-21 09:49:42 -0400

    finish async

--- app/async.js
+++ app/async.js
@@ -1,11 +1,20 @@
 exports = typeof window === 'undefined' ? global : window;
-
 exports.asyncAnswers = {
-  async: function(value) {
-
+  async: function (value) {
+    var deferred = $.Deferred();
+    setTimeout(function () {
+      deferred.resolve(value);
+    }, 20);
+    return deferred.promise();
   },
-
-  manipulateRemoteData: function(url) {
-
+  manipulateRemoteData: function (url) {
+    var promise = $.Deferred();
+    $.ajax(url).then(function (resp) {
+      var people = resp.people.map(function (person) {
+        return person.name;
+      });
+      promise.resolve(people.sort());
+    });
+    return promise.promise();
   }
 };
